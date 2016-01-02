340014ab2d82049e932e980bf26f9c7b5217fd1e
akmonk <akmonk@mail.ru>
2016-01-02 18:15:05 +0300

    add replace array

--- index.php
+++ index.php
@@ -36,7 +36,7 @@
 			')' => '' 
 		);
 		//$new_name = str_replace_assoc($replace,$row['name']);
-		//$row['name'] = iconv('UTF-8','cp1251',$row['name']);
+		$row['name'] = iconv('UTF-8','cp1251',$row['name']);
 		//$new_name = preg_replace('/([.+{},®!-]+)/', '_', $row['name']); 		
 		$new_name = preg_replace('/([-.+{},®!-"?:^~|@¹$–=*&%;<>()—«»#\/\']+)/', '_', $row['name']);
 		$new_name = preg_replace('/\s\s+/', ' ', $new_name);		
@@ -44,10 +44,11 @@
 		$new_name = preg_replace('/__+/', '_', $new_name);
 		$new_name = rtrim ($new_name,"...");
 		echo "<pre>";var_dump($row);"</pre>";
+		$new_name = iconv('cp1251','UTF-8',$new_name);
 		$sql2 = "UPDATE `shares` SET `name` = \"{$new_name}\" WHERE `id`={$row['id']};";
 		echo $sql2."<br/>";
 		$result2 = $dbh->query($sql2);
-		//$new_name = iconv('cp1251','UTF-8',$new_name);
+		
 		echo $new_name."<br/>";
 	}
 	
