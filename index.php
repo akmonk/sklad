9cbfbd37d58847c773015565161c8f5fb6afe985
akmonk <akmonk@mail.ru>
2016-01-02 13:03:10 +0300

    add db class and test query

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
 <?php
-echo "true";
-?>
+	require_once "database.php";
+	
+	$dbh=new CDataBase("skladchik", "localhost", "ypbase", "golosneba"); 
+
+	$table = 'prepare_csv';
+	
+	$query = "SELECT * FROM {$table} WHERE 1";
+		// Выполняем запрос к БД
+	$result = $dbh->query($query);
+	
+	while ($row = $dbh->fetch_array($result)) 
+	{
+		echo $row['name']. "<br/>";
+	}
+	?>
