541cc255ae0535383d77940866d1aa848e3333d3
akmonk <akmonk@mail.ru>
2016-01-02 14:48:30 +0300

    add str_replace_assoc

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 <?php
 	require_once "database.php";
 	
+	function str_replace_assoc(array $replace, $subject) 
+	{ 
+		return str_replace(array_keys($replace), array_values($replace), $subject);    
+	}
+	
 	$dbh=new CDataBase("skladchik", "localhost", "ypbase", "golosneba"); 
 
 	$table = 'prepare_csv';
